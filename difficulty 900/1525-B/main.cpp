8d21cc3a946ef61d453324f622a3e3046564fa2a
Louay Farah <86551620+louayfarah@users.noreply.github.com>
2021-08-05 11:37:38 +0100

    Add files via upload

--- difficulty 900/1525-B/main.cpp
+++ difficulty 900/1525-B/main.cpp
@@ -1,1 +1,70 @@
+#include "bits/stdc++.h"
 
+using namespace std;
+
+#define endl "\n"
+
+const long long MOD = 1e9+7;
+const long long INF = 1e18;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+        for(int i = 0; i<n; i++)
+            cin >> arr[i];
+
+        vector<int> sorted_arr;
+        for(int i = 1; i<=n; i++)
+            sorted_arr.push_back(i);
+
+        int nb = 0, res = 0;
+
+        if(arr[0]==n)
+            nb++;
+        if(arr[n-1]==1)
+            nb++;
+        if(arr[0]==1)
+            nb--;
+        if(arr[n-1]==n)
+            nb--;
+
+        bool sorted = true;
+
+        for(int i = 0; i<n; i++)
+        {
+            if(arr[i]!=sorted_arr[i])
+            {
+                sorted = false;
+                break;
+            }
+        }
+
+        if(sorted)
+            res = 0;
+        else if(nb<0)
+            res = 1;
+        else if(nb<2)
+            res = 2;
+        else if(nb==2)
+            res = 3;
+
+        cout << res << endl;
+    }
+
+    return 0;
+}
