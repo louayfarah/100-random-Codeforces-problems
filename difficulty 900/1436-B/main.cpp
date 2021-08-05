a265663468443c4e94e5f1e21736b375c1e52b2a
Louay Farah <86551620+louayfarah@users.noreply.github.com>
2021-08-05 11:35:52 +0100

    Add files via upload

--- difficulty 900/1436-B/main.cpp
+++ difficulty 900/1436-B/main.cpp
@@ -1,1 +1,69 @@
+#include "bits/stdc++.h"
 
+using namespace std;
+
+#define endl "\n"
+
+const long long MOD = 1e9+7;
+const long long INF = 1e18;
+
+
+bool is_prime(int x)
+{
+    if(x==1)
+        return false;
+
+    for(int i = 2; i*i<=x; i++)
+    {
+        if(x%i==0)
+            return false;
+    }
+
+    return true;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<vector<int>> grid(n, vector<int>(n, 1));
+
+        if(!is_prime(n))
+        {
+            int curr = n-1;
+            int i = 1;
+            while(true)
+            {
+                if(!is_prime(i)&&is_prime(curr+i))
+                    break;
+                i++;
+            }
+
+            for(int j = 0; j<n; j++)
+            {
+                grid[j][j] = i;
+            }
+        }
+
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = 0; j<n; j++)
+                cout << grid[i][j] << ' ';
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
