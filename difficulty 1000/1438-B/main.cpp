d7b684f97dd131525643237d18b660bcd39f62f9
Louay Farah <86551620+louayfarah@users.noreply.github.com>
2021-08-08 11:28:41 +0100

    Add files via upload

--- difficulty 1000/1438-B/main.cpp
+++ difficulty 1000/1438-B/main.cpp
@@ -1,1 +1,50 @@
+#include "bits/stdc++.h"
 
+using namespace std;
+
+#define endl "\n"
+
+const long long MOD = 1e9+7;
+const long long INF = 1e18;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<long long> b(n);
+        for(int i = 0; i<n; i++)
+            cin >> b[i];
+
+        sort(b.begin(), b.end());
+
+        bool flag = false;
+        for(int i = 1; i<n; i++)
+        {
+            if(b[i]==b[i-1])
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if(flag)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
