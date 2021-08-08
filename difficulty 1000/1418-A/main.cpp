b8b0804877d98f39d641b2ed4685554c01fe8a6a
Louay Farah <86551620+louayfarah@users.noreply.github.com>
2021-08-08 11:28:24 +0100

    Add files via upload

--- difficulty 1000/1418-A/main.cpp
+++ difficulty 1000/1418-A/main.cpp
@@ -1,1 +1,39 @@
+#include "bits/stdc++.h"
 
+using namespace std;
+
+#define endl "\n"
+
+const long long MOD = 1e9+7;
+const long long INF = 1e18;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        long long x, y, k;
+        cin >> x >> y >> k;
+
+        long long needed = y*k+k-1;
+
+        long long res = 0;
+        res+=k;
+        res+=needed/(x-1);
+        if(needed%(x-1)!=0)
+            res++;
+
+        cout << res << endl;
+    }
+
+    return 0;
+}
