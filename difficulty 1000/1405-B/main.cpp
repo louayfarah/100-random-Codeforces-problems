8c673262f4a6a1a1d1afb5e1a25dc519ebc2ba4b
Louay Farah <86551620+louayfarah@users.noreply.github.com>
2021-08-08 11:27:48 +0100

    Add files via upload

--- difficulty 1000/1405-B/main.cpp
+++ difficulty 1000/1405-B/main.cpp
@@ -1,1 +1,44 @@
+#include "bits/stdc++.h"
 
+using namespace std;
+
+#define endl "\n"
+
+const long long MOD = 1e9+7;
+const long long INF = 1e18;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<long long> arr(n);
+        for(int i = 0; i<n; i++)
+            cin >> arr[i];
+
+        long long mini = 0;
+        long long curr = 0;
+
+        for(int i = 0; i<n; i++)
+        {
+            curr+=arr[i];
+            mini = min(mini, curr);
+        }
+
+        cout << -mini << endl;
+    }
+
+    return 0;
+}
