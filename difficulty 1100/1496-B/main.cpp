6efa0b4957285e2bd42329b4325835cca83e346e
Louay Farah <86551620+louayfarah@users.noreply.github.com>
2021-08-13 09:40:59 +0100

    Add files via upload

--- difficulty 1100/1496-B/main.cpp
+++ difficulty 1100/1496-B/main.cpp
@@ -1,1 +1,60 @@
+#include "bits/stdc++.h"
 
+using namespace std;
+
+#define endl "\n"
+
+const long long MOD = 1e9+7;
+const long long INF = 1e18;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        long long k;
+        cin >> n >> k;
+
+        vector<long long> arr(n);
+        for(int i = 0; i<n; i++)
+            cin >> arr[i];
+
+        sort(arr.begin(), arr.end());
+
+        long long maxi = arr[n-1], mex = 0;
+        for(int i = 0; i<n; i++)
+        {
+            if(arr[i]==mex)
+                mex++;
+            else
+                break;
+        }
+
+        if(k==0)
+            cout << n << endl;
+        else if(mex>maxi)
+            cout << k+n << endl;
+        else
+        {
+            long long searched = ceil(double(mex+maxi)/2);
+            bool flag = binary_search(arr.begin(), arr.end(), searched);
+
+            if(flag)
+                cout << n << endl;
+            else
+                cout << n+1 << endl;
+        }
+    }
+
+    return 0;
+}
