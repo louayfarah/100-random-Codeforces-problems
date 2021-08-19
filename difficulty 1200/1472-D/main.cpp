84f0e0cf2e374e8b8cc6eda9142d81a5d9ec0416
Louay Farah <86551620+louayfarah@users.noreply.github.com>
2021-08-19 09:17:04 +0100

    Add files via upload

--- difficulty 1200/1472-D/main.cpp
+++ difficulty 1200/1472-D/main.cpp
@@ -1,1 +1,66 @@
+#include "bits/stdc++.h"
 
+using namespace std;
+
+#define endl "\n"
+
+const long long MOD = 1e9+7;
+const long long INF = 1e18;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<long long> arr;
+
+        for(int i = 0; i<n; i++)
+        {
+            long long x;
+            cin >> x;
+
+            arr.push_back(x);
+        }
+
+        sort(arr.begin(), arr.end(), greater<long long>());
+
+        long long res = 0;
+        bool flag = true;
+        for(int i = 0; i<n; i++)
+        {
+            if(flag)
+            {
+                if(arr[i]%2==0)
+                    res+=arr[i];
+            }
+            else
+            {
+                if(arr[i]%2==1)
+                    res-=arr[i];
+            }
+
+            flag = !flag;
+        }
+
+        if(res>0)
+            cout << "Alice" << endl;
+        else if(res<0)
+            cout << "Bob" << endl;
+        else
+            cout << "Tie" << endl;
+    }
+
+    return 0;
+}
