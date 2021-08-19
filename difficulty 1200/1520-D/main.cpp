daf26b73d0c73482de4d152e89980d48c1734fac
Louay Farah <86551620+louayfarah@users.noreply.github.com>
2021-08-19 09:18:07 +0100

    Add files via upload

--- difficulty 1200/1520-D/main.cpp
+++ difficulty 1200/1520-D/main.cpp
@@ -1,1 +1,62 @@
+#include "bits/stdc++.h"
 
+using namespace std;
+
+#define endl "\n"
+
+const long long MOD = 1e9+7;
+const long long INF = 1e18;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<long long> arr(n);
+        for(int i = 0; i<n; i++)
+            cin >> arr[i];
+
+        for(int i = 0; i<n; i++)
+        {
+            arr[i]-=i+1;
+        }
+
+        sort(arr.begin(), arr.end());
+
+        long long mini = arr[0];
+        if(arr[0]<0)
+            for(int i = 0; i<n; i++)
+                arr[i]+=-mini;
+
+        vector<bool> visited(2*n);
+        long long res = 0;
+
+        for(int i = 0; i<n; i++)
+        {
+            long long elt = arr[i];
+            if(visited[elt])
+                continue;
+
+            long long nb = upper_bound(arr.begin(), arr.end(), elt) - lower_bound(arr.begin(), arr.end(), elt);
+            nb = nb*(nb-1)/2;
+            res+=nb;
+            visited[elt] = true;
+        }
+
+        cout << res << endl;
+    }
+
+    return 0;
+}
