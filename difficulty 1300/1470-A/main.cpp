eadee13c6a1deda1a485692e64dbd06d6ff1fc0d
Louay Farah <86551620+louayfarah@users.noreply.github.com>
2021-09-20 17:34:00 +0100

    Add files via upload

--- difficulty 1300/1470-A/main.cpp
+++ difficulty 1300/1470-A/main.cpp
@@ -1,1 +1,61 @@
+#include "bits/stdc++.h"
 
+using namespace std;
+
+#define endl "\n"
+
+const long long MOD = 1e9+7;
+const long long INF = 1e18;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        vector<int> friends(n);
+        vector<long long> presents(m);
+
+        for(int i = 0; i<n; i++)
+        {
+            cin >> friends[i];
+            friends[i]--;
+        }
+        for(int i = 0; i<m; i++)
+            cin >> presents[i];
+
+        sort(friends.begin(), friends.end(), greater<int>());
+
+        int i = 0;
+
+        long long res = 0;
+        while(i<n&&i<m)
+        {
+            if(presents[i]>presents[friends[i]])
+                break;
+            res+=presents[i];
+            i++;
+        }
+
+        while(i<n)
+        {
+            res+=presents[friends[i]];
+            i++;
+        }
+
+        cout << res << endl;
+    }
+
+    return 0;
+}
